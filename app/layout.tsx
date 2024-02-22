75760f861480780a29f9427ec560cae7608fa954
Frango113 <frangoat38@aol.com>
2024-02-22 17:40:40 -0300

    nav somewhat completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,9 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-indigo-950 text-indigo-50 relative`}>{children}
-      <div className="bg-[#f2cd60] absolute top-[-6ream] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[25rem] sm:w-[68.75rem]"></div>
-      <div className="bg-[#0D0B08] absolute top-[-6ream] -z-10 left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[25rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] x1:left-[-15rem] 2x1:left-[-5rem]"></div>
+      <body className={`${inter.className} bg-indigo-950 text-indigo-50 relative
+      h-[5000px]`}>
+      <div className="bg-[#f2cd60] absolute top-[-6ream] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] 
+      rounded-full blur-[25rem] sm:w-[68.75rem]"></div>
+      <div className="bg-[#0D0B08] absolute top-[-6ream] -z-10 left-[-35rem] h-[31.25rem] w-[31.25rem] 
+      rounded-full blur-[25rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem]
+       x1:left-[-15rem] 2x1:left-[-5rem]"></div>
+       {children}
+       <Header/>
       </body>
     </html>
   )
