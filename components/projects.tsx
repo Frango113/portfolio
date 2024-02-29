f13d2a8acd75910832039037d4a2322d3e0d607d
Frango113 <frangoat38@aol.com>
2024-02-29 17:05:10 -0300

    29/2 2

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import SectionHeading from './section-heading'
+import { projectsData } from '@/lib/data'
 
 export default function Projects() {
   return (
     <section><SectionHeading>My Projects
-        </SectionHeading></section>
+        </SectionHeading>
+        <div>
+            {
+                projectsData.map((project, index)=>(
+                    <React.Fragment key={index}>
+                      <Project {...project} />
+                    </React.Fragment>
+            ))}
+        </div>
+    </section>
   )
+}
+
+type ProjectProps= (typeof projectsData)[number];
+
+function Project({
+    title,
+    description,
+    tags,
+    ProjectImage
+}:ProjectProps){
+    /* const imageUrl = typeof ProjectImage === 'string' ? ProjectImage : ProjectImage.default */
+    return( 
+    <section className="bg-indigo-800 max-w-[42rem] border
+     border-white/5 overflow-hidden sm:pr-8 relative">
+        <h3 className="text-2x1 font-semibold">{title}</h3>
+        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+        <ul className="flex flex-wrap mt-4 gap-2">
+            {tags.map((tag,index)=>(
+                <li className="bg-indigo-50 px-3 py-1 text-[0.7rem] 
+                uppercase tracking-wider text-indigo-800 rounded-full" key={index}>{tag}</li>
+            ))}
+            </ul>
+            <img src={ProjectImage.src} alt={title} className='absoulte top-8 -right-40'></img>
+            </section>
+    )
 }
--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,17 @@
 import React from "react";
+import {CgWorkAlt} from "react-icons/cg"
+import {FaReact} from "react-icons/fa"
+import {LuGraduationCap} from "react-icons/lu"
+import TripInsight from "../components/assets/TripInsight.jpeg"
+import DogsApp from "../components/assets/DogsApp.jpeg"
+import { StaticImageData } from "next/image";
+
+interface ProjectData {
+    title: string;
+    description: string;
+    tags: string[];
+    ProjectImage: StaticImageData; // Assuming these are image imports, you might need to adjust the type
+}
 
 export const links=[
     {
@@ -33,6 +46,7 @@ export const experiencesData=[
         title: "Fullstack developer, Henry Bootcamp",
         location: "Argentina- Remote",
         description: "Graduated with 800 hours of theoretical-practical study",
+        icon: React.createElement(LuGraduationCap),
         date: "2023-2024"
     },
     {
@@ -49,23 +63,26 @@ export const experiencesData=[
     },
 ] as const
 
-export const projectsData=[
+export const projectsData:ProjectData[]=[
     {
         title: "Trip In Sight",
         description: "I worked as a Front-End Engineer, developing the web app using React.js for the user interface, Integrated Paypal api to the App",
-        tags:["React", "JavaScript", "Redux-Toolkit", "Tailwind", "PayPal Api"]
+        tags:["React", "JavaScript", "Redux-Toolkit", "Tailwind", "PayPal Api"],
+        ProjectImage:TripInsight
 
     },
     {
         title: "K9's",
         description: "Designed and developed the app, both server side and client side, a simple SPA that gives the user information about different dog breeds",
-        tags:["React", "JavaScript", "Redux", "CSS", "Express", "Sequelize"]
+        tags:["React", "JavaScript", "Redux", "CSS", "Express", "Sequelize"],
+        ProjectImage: DogsApp
 
     },
     {
         title: "KTR dealer App",
         description: "simple database management app, created with Python, Tkinter and SQLite",
-        tags: ["Python", "Tkinter", "SQLite"]
+        tags: ["Python", "Tkinter", "SQLite"],
+        ProjectImage: DogsApp
     }
 ] as const;
 export const skillsData =[
