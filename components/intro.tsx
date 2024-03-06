6175a864015d7995886dd032797ab33ccfe5ab59
Frango113 <frangoat38@aol.com>
2024-03-06 16:54:54 -0300

    added some styles

--- components/intro.tsx
+++ components/intro.tsx
@@ -72,7 +72,9 @@ export default function Intro() {
         }}>
 <Link
           href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          className="bg-indigo-100 text-indigo-700 px-7 py-3 flex items-center gap-2 
+            rounded-full outline-none focus:scale-110 hover:scale-110
+             hover:bg-indigo-700 hover:text-white transition-all"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
